bff06bf298d4490bc31adee9885716154fca4c6c
Nil Portugues Caldero <nil@letgo.com>
2016-04-27 16:50:38 +0200

    SqlRepositoryHydrator added

--- example/UserRepository.php
+++ example/UserRepository.php
@@ -10,66 +10,13 @@
  */
 namespace NilPortugues\Example\Repository;
 
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Fields;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Filter;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Identity;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Pageable;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Sort;
-use NilPortugues\Foundation\Domain\Model\Repository\Page;
 use NilPortugues\Foundation\Infrastructure\Model\Repository\Sql\SqlRepository;
+use NilPortugues\Foundation\Infrastructure\Model\Repository\Sql\SqlRepositoryHydrator;
 
 /**
  * Class UserRepository.
  */
 class UserRepository extends SqlRepository
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function find(Identity $id, Fields $fields = null)
-    {
-        $result = parent::find($id, $fields);
-
-        return $this->mapping->fromArray($result);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findBy(Filter $filter = null, Sort $sort = null, Fields $fields = null)
-    {
-        $result = parent::findBy($filter, $sort, $fields);
-
-        return $this->hydrateArray($result);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findAll(Pageable $pageable = null)
-    {
-        $page = parent::findAll($pageable);
-
-        return new Page(
-            $this->hydrateArray($page->content()),
-            $page->totalElements(),
-            $page->pageNumber(),
-            $page->totalPages(),
-            $page->sortings(),
-            $page->filters(),
-            $page->fields()
-        );
-    }
-
-    /**
-     * @param array $result
-     *
-     * @return array
-     */
-    public function hydrateArray(array $result)
-    {
-        return array_map(function ($v) {
-            return $this->mapping->fromArray($v);
-        }, $result);
-    }
+    use SqlRepositoryHydrator;
 }
--- tests/CustomerRepository.php
+++ tests/CustomerRepository.php
@@ -2,73 +2,10 @@
 
 namespace NilPortugues\Tests\Foundation;
 
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Fields;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Filter;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Identity;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Pageable;
-use NilPortugues\Foundation\Domain\Model\Repository\Contracts\Sort;
-use NilPortugues\Foundation\Domain\Model\Repository\Page;
 use NilPortugues\Foundation\Infrastructure\Model\Repository\Sql\SqlRepository;
+use NilPortugues\Foundation\Infrastructure\Model\Repository\Sql\SqlRepositoryHydrator;
 
 class CustomerRepository extends SqlRepository
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function find(Identity $id, Fields $fields = null)
-    {
-        $result = parent::find($id, $fields);
-
-        return ($result) ? $this->mapping->fromArray($result) : [];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findBy(Filter $filter = null, Sort $sort = null, Fields $fields = null)
-    {
-        $results = parent::findBy($filter, $sort, $fields);
-
-        return ($results) ? $this->hydrateArray($results) : [];
-    }
-
-    /**
-     * @param array $results
-     *
-     * @return array
-     */
-    protected function hydrateArray(array $results)
-    {
-        return array_map(function ($result) {
-            return $this->mapping->fromArray($result);
-        }, $results);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findAll(Pageable $pageable = null)
-    {
-        $page = parent::findAll($pageable);
-
-        return new Page(
-            ($page->content()) ? $this->hydrateArray($page->content()) : [],
-            $page->totalElements(),
-            $page->pageNumber(),
-            $page->totalPages(),
-            $page->sortings(),
-            $page->filters(),
-            $page->fields()
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function findByDistinct(Fields $distinctFields, Filter $filter = null, Sort $sort = null)
-    {
-        $results = parent::findByDistinct($distinctFields, $filter, $sort);
-
-        return ($results) ? $this->hydrateArray($results) : [];
-    }
+    use SqlRepositoryHydrator;
 }
